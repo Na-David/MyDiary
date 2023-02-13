5a7b160c9b33360318df4e00c4f1d3936d3d8f8a
David Na <hna206@naver.com>
2023-02-13 16:14:07 +1300

    added asset imgs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import RouteTest from './components/RouteTest';
-
 import Home from './pages/Home'
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       <div className="App">
         <h2>Hello</h2>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
@@ -20,7 +26,6 @@ function App() {
           <Route path='/diary' element={<Diary />} />
           <Route path='/diary/:id' element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
