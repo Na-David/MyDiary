57dd1baf8371ea46aa6a007b93a6cb3c35e2457c
David Na <hna206@naver.com>
2023-02-15 01:13:29 +1300

    sorting data

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,8 +1,49 @@
+import {useState} from 'react';
+
+const sortOptionList = [
+    {value : "lastest", name : "Lastest Order"},
+    {value : "oldest", name : "Oldest Order"}
+];
+
+const ControlMenu = ({value, onChange, optionList}) => {
+    return (
+    <select value={value} onChange = {(e) => onChange(e.target.value)}>
+        {optionList.map((it,idx) => (
+        <option key={idx} value={it.value}>
+            {it.name}
+        </option>))}
+    </select>
+)};
+
 const DiaryList = ({diaryList}) => {
-    return <div>
-        {diaryList.map((it) => (<div key={it.id}>{it.content}</div>))}
-    </div>
-}
+
+    const [sortType, setSortType] = useState("Lastest");
+    const getProcessedDiaryList = () => {
+
+        const compare = (a,b) => {
+            if (sortType === 'latest'){
+                return parseInt(b.date) - parseInt(a.date);
+            } else {
+                return parseInt(a.date) - parseInt(b.date);
+            }
+        }
+
+        const copyList = JSON.parse(JSON.stringify(diaryList));
+        const sortedList = copyList.sort(compare);
+        return sortedList;
+    }
+
+    return (
+        <div>
+            <ControlMenu 
+                value={sortType} 
+                ocChange = {setSortType} 
+                optionList = {sortOptionList}
+            />
+            {diaryList.map((it) => (<div key={it.id}>{it.content}</div>))}
+        </div>
+    );
+};
 
 DiaryList.defaultProps = {
     diaryList : []
