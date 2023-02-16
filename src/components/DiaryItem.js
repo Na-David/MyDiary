b8d2ae7bf755ba20b2cfa39a7369ed505b9d8845
David Na <hna206@naver.com>
2023-02-16 23:30:45 +1300

    diary item mid col

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,9 +1,15 @@
 const DiaryItem = ({id, emotion, content, date}) => {
+
+    const strDate = new Date(parseInt(date)).toLocaleDateString();
+
     return <div className="DiaryItem">
         <div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")}>
             <img src= {process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
         </div>
-        <div></div>
+        <div className="info_wrapper">
+            <div className="diary_date">{strDate}</div>
+            <div className="diary_content_preview">{content.slice(0,25)}</div>
+        </div>
         <div></div>
     </div>
 }
