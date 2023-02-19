1df59d230763f46e5c328c7e05a4961c2cbc620a
David Na <hna206@naver.com>
2023-02-20 01:54:53 +1300

    Diary detail page

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -50,6 +50,12 @@ const Diary = () => {
                         </div>
                     </div>
                 </section>
+                <section>
+                    <h4>Today's Diary</h4>
+                    <div className='diary_content_wrapper'>
+                        <p>{data.content}</p>
+                    </div>
+                </section>
             </article>
         </div>
     )}
