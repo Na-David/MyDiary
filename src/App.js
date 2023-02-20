1fdcf58b1ffb98ec42a0ac5e5132845e1752d717
David Na <hna206@naver.com>
2023-02-21 02:14:42 +1300

    Local Storage

--- src/App.js
+++ src/App.js
@@ -42,6 +42,16 @@ const App = () => {
 
   const [data, dispatch] = useReducer(reducer,[]);
 
+  useEffect(() => {
+    const localData = localStorage.getItem('diary');
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort((a,b) => parseInt(b.id) - parseInt(a.id));
+      dataId.current = parseInt(diaryList[0].id) + 1;
+
+      dispatch({type : "INIT", data: diaryList});
+    }
+  },[])
+
   const dataId = useRef(0);
   //CREATE, DELETE, EDIT
 
